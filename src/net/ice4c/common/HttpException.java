23b3d62781367194f198904c70e48111ef34fddc
ice4c <ccav@qq.com>
2013-05-10 13:53:18 +0800

    提交看看

---
+++ src/net/ice4c/common/HttpException.java
@@ -0,0 +1,25 @@
+package net.ice4c.common;
+
+public class HttpException extends RuntimeException {
+
+	public HttpException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public HttpException(String detailMessage, Throwable throwable) {
+		super(detailMessage, throwable);
+		// TODO Auto-generated constructor stub
+	}
+
+	public HttpException(String detailMessage) {
+		super(detailMessage);
+		// TODO Auto-generated constructor stub
+	}
+
+	public HttpException(Throwable throwable) {
+		super(throwable);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/net/ice4c/tuer/MyFriendsDailyListActivity.java
@@ -0,0 +1,15 @@
+package net.ice4c.tuer;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MyFriendsDailyListActivity extends Activity {
+	/**
+	 * @see android.app.Activity#onCreate(Bundle)
+	 */
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		// TODO Put your code here
+	}
+}
